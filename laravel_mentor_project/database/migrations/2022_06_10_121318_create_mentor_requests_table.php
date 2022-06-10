fcd5a86e4d57a36e7088d8c1872b8280a8e944f6
khozamaObeidat <100359224+khozamaObeidat@users.noreply.github.com>
2022-06-10 15:54:23 +0300

    first

---
+++ laravel_mentor_project/database/migrations/2022_06_10_121318_create_mentor_requests_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMentorRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mentor_requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('mentor_firstname');
+            $table->string('mentor_lastname');
+            $table->string('mentor_email');
+            $table->string('mentor_password');
+            $table->bigInteger('mentor_phone_number');
+
+            $table->text('mentor_image');
+            $table->bigInteger('mentor_category')->unsigned()->nullable();
+            $table->text('mentor_about');
+
+            $table->foreign('mentor_category')->references('id')->on('services')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mentor_requests');
+    }
+}
