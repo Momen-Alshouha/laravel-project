02935739a8aa786b8c811e5ab8a44eac191d5bbc
khozamaObeidat <100359224+khozamaObeidat@users.noreply.github.com>
2022-06-11 21:47:21 +0300

    admindashbourd_ready

--- project6/routes/web.php
+++ project6/routes/web.php
@@ -43,6 +43,26 @@ Route::get('/dashboard', function () {
     return view('admin.dashboard');
 });
 
+
+Route::get('/requests', function () {
+    return view('admin.requests');
+});
+
+
+Route::get('/category', function () {
+    return view('admin.category');
+});
+
+Route::get('/approved', function () {
+    return view('admin.Approved');
+});
+
+Route::get('/addcategory', function () {
+    return view('admin.addcategory');
+});
+
+
+
 Auth::routes();
 
 Route::get('/auth', [App\Http\Controllers\HomeController::class, 'index'])->name('auth');
