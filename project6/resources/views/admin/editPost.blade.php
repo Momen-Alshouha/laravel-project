d5a6b505ec94317a7c737b179b6321c0706cf027
Esraa-Khalila <82060330+Esraa-Khalila@users.noreply.github.com>
2022-06-13 20:54:51 +0300

    edit value

--- project6/resources/views/admin/editPost.blade.php
+++ project6/resources/views/admin/editPost.blade.php
@@ -88,9 +88,9 @@
                     <div class="form-group">
                         <strong>description:</strong>
                         <select name='active'>
-                      
+                        <option value=''></option>
                         <option value='{{$post->active}}'>{{$post->active}}</option>
-                         <option value='non-active'>active</option>
+                         <option value='active'>active</option>
                         <select>
                     </div>
                 </div>
